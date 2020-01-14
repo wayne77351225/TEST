2e8a5a5c46d3fd9be8f5a59bf3ef0ab26e701c97
muchone1981 <muchone1981@gmail.com>
2020-01-14 17:38:54 +0800

    語言設定/fontb設定/馬達加速設定/濃度調節設定

--- G80Utility/G80MainWindow.xaml.cs
+++ G80Utility/G80MainWindow.xaml.cs
@@ -79,6 +79,7 @@ namespace PirnterUtility
             registerUSBdetect();
         }
 
+
         #region UI初始化
         private void UIInitial()
         {
@@ -88,21 +89,31 @@ namespace PirnterUtility
             DataContext = viewmodel;
 
             //BaudRate預設
-            BaudRateCom.SelectedIndex = 3;
-            App.Current.Properties["BaudRateSetting"] = 115200;
+            BaudRateCom.SelectedIndex = 2;
+            App.Current.Properties["BaudRateSetting"] = 38400;
+            
             //default 為選取RS232
             this.RS232Radio.IsChecked = true;
 
+            //combobox設定item
+            //Density
+            List<int> DensityList = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                DensityList.Add(i);
+            }
+            DensityCom.ItemsSource = DensityList;
+            //CodePage
+            CodePageCom.ItemsSource = CodePage.getCodePageList();
+
         }
         #endregion
-
-
 
         //========================Btn點擊事件===========================
         #region 通讯接口测试按鈕事件
         private void ConnectTest_Click(object sender, RoutedEventArgs e)
         {
-            byte[] sendArray = StringToByteArray(Command.RS232_COMMUNITCATION_TEST);
+            byte[] sendArray = StringToByteArray(Command.RS232_COMMUNICATION_TEST);
             if ((bool)rs232Checkbox.IsChecked)
             {
                 SerialPortConnect("CommunicationTest", sendArray);
@@ -118,8 +129,8 @@ namespace PirnterUtility
             switch (DeviceType)
             {
 
-                case "RS232":                   
-                    SerialPortConnect("Beep", sendArray);
+                case "RS232":
+                    SerialPortConnect("BeepOrSetting", sendArray);
                     break;
                 case "USB":
 
@@ -132,20 +143,91 @@ namespace PirnterUtility
         }
         #endregion
 
-        #region 走紙方向
+        #region 設定語言按鈕事件
+        private void LanguageSetBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            byte[] sendArray = null;
+
+            switch (LanguageSetCom.SelectedIndex)
+            {
+                case 0:
+                    sendArray = StringToByteArray(Command.LANGUAGE_SETTING_GB18030);
+                    break;
+                case 1:
+                    sendArray = StringToByteArray(Command.LANGUAGE_SETTING_BIG5);
+                    break;
+                case 2:
+                    sendArray = StringToByteArray(Command.LANGUAGE_SETTING_KOREAN);
+                    break;
+                case 3:
+                    sendArray = StringToByteArray(Command.LANGUAGE_SETTING_JAPANESE);
+                    break;
+                case 4:
+                    sendArray = StringToByteArray(Command.LANGUAGE_SETTING_SHIFT_JIS);
+                    break;
+                case 5:
+                    sendArray = StringToByteArray(Command.LANGUAGE_SETTING_JIS);
+                    break;
+            }
+            switch (DeviceType)
+            {
+                case "RS232":
+                    SerialPortConnect("BeepOrSetting", sendArray);
+                    break;
+                case "USB":
+
+                    break;
+                case "Ethernet":
+
+                    break;
+            }
+        }
+        #endregion
+
+        #region FontB設定按鈕事件
+        private void FontBSettingBtn_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] sendArray;
+            if (FontBSettingCom.SelectedIndex == 0)
+            {
+                sendArray = StringToByteArray(Command.FONTB_OFF_SETTING);
+            }
+            else
+            {
+                sendArray = StringToByteArray(Command.FONTB_ON_SETTING);
+            }
+            switch (DeviceType)
+            {
+                case "RS232":
+                    SerialPortConnect("BeepOrSetting", sendArray);
+                    break;
+                case "USB":
+
+                    break;
+                case "Ethernet":
+
+                    break;
+            }
+        }
+        #endregion
+
+        #region 走紙方向按鈕事件
         private void Direction_Click(object sender, RoutedEventArgs e)
         {
             byte[] sendArray;
+            if (DirectionCombox.SelectedIndex == 0)
+            {
+                sendArray = StringToByteArray(Command.DIRECTION_H80250N_SETTING);
+            }
+            else
+            {
+                sendArray = StringToByteArray(Command.DIRECTION_80250N_SETTING);
+            }
             switch (DeviceType)
-            {         
-                case "RS232":                   
-                    if (DirectionCombox.SelectedIndex == 0) {
-                        sendArray = StringToByteArray(Command.Direction_H80250N);
-                    }
-                   else{
-                        sendArray = StringToByteArray(Command.Direction_80250N);
-                    }                   
-                    SerialPortConnect("Beep", sendArray);
+            {
+                case "RS232":
+                    SerialPortConnect("BeepOrSetting", sendArray);
                     break;
                 case "USB":
 
@@ -156,6 +238,100 @@ namespace PirnterUtility
             }
         }
         #endregion
+
+        #region 設定馬達加速度按鈕事件
+        private void AccMotorBtn_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] sendArray = null;
+
+            switch (AccMotorCom.SelectedIndex)
+            {
+                case 0: //1.0=>01
+                    sendArray = StringToByteArray(Command.ACCELERATION_OF_MOTOR_SETTING +"01");
+                    break;
+                case 1: //0.8=>02
+                    sendArray = StringToByteArray(Command.ACCELERATION_OF_MOTOR_SETTING + "02");
+                    break;
+                case 2: //0.6=>03
+                    sendArray = StringToByteArray(Command.ACCELERATION_OF_MOTOR_SETTING + "03");
+                    break;
+                case 3: //0.4=>04
+                    sendArray = StringToByteArray(Command.ACCELERATION_OF_MOTOR_SETTING + "04");
+                    break;
+                case 4: //0.2=>05
+                    sendArray = StringToByteArray(Command.ACCELERATION_OF_MOTOR_SETTING + "05");
+                    break;
+            }
+            switch (DeviceType)
+            {
+                case "RS232":
+                    SerialPortConnect("BeepOrSetting", sendArray);
+                    break;
+                case "USB":
+
+                    break;
+                case "Ethernet":
+
+                    break;
+            }
+
+        }
+        #endregion
+
+        #region 設定濃度調節鈕事件
+        private void DensityBtn_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] sendArray = null;
+
+            switch (DensityCom.SelectedIndex)
+            {
+                case 0: //1=>16
+                    sendArray = StringToByteArray(Command.DENSITY_SETTING_HEADER + "16");
+                    break;
+                case 1: //2=>61
+                    sendArray = StringToByteArray(Command.DENSITY_SETTING_HEADER + "61");
+                    break;
+                case 2: //3=>01
+                    sendArray = StringToByteArray(Command.DENSITY_SETTING_HEADER + "01");
+                    break;
+                case 3: //4=>02
+                    sendArray = StringToByteArray(Command.DENSITY_SETTING_HEADER + "02");
+                    break;
+                case 4: //5=>03
+                    sendArray = StringToByteArray(Command.DENSITY_SETTING_HEADER + "03");
+                    break;
+                case 5: //6=>04
+                    sendArray = StringToByteArray(Command.DENSITY_SETTING_HEADER + "04");
+                    break;
+                case 6: //7=>05
+                    sendArray = StringToByteArray(Command.DENSITY_SETTING_HEADER + "05");
+                    break;
+                case 7: //8=>06
+                    sendArray = StringToByteArray(Command.DENSITY_SETTING_HEADER + "06");
+                    break;
+                case 8: //9=>07
+                    sendArray = StringToByteArray(Command.DENSITY_SETTING_HEADER + "07");
+                    break;
+                case 9: //10=>08
+                    sendArray = StringToByteArray(Command.DENSITY_SETTING_HEADER + "08");
+                    break;
+            }
+            switch (DeviceType)
+            {
+                case "RS232":
+                    SerialPortConnect("BeepOrSetting", sendArray);
+                    break;
+                case "USB":
+
+                    break;
+                case "Ethernet":
+
+                    break;
+            }
+
+        }
+        #endregion
+
         //========================取得資料後設定UI=================
 
         #region 設定打印機型號/軟件版本/機器序號
@@ -236,50 +412,37 @@ namespace PirnterUtility
             if (RS232PortName != null)
             {
                 bool isError = RS232Connect.OpenSerialPort(RS232PortName, FindResource("CannotOpenComport") as string);
-                
+
                 if (!isError)
                 {
-                    switch (dataType )
+                    switch (dataType)
                     {
                         case "PrinterInfo":
-                        bool isReceiveData = RS232Connect.SerialPortSendCMD("NeedReceive", data, null, 8);
-                        while (!isReceiveData)
-                        {
-
-                            if (RS232Connect.mRecevieData != null)
+                            bool isReceiveData = RS232Connect.SerialPortSendCMD("NeedReceive", data, null, 8);
+                            while (!isReceiveData)
                             {
-                                //Console.WriteLine("result:" + Encoding.Default.GetString(RS232Connect.mRecevieData));
-                                switch (dataType)
+
+                                if (RS232Connect.mRecevieData != null)
                                 {
-                                    case "PrinterInfo":
-                                        //byte[] TETS = StringToByteArray("1F1B1F48461101005F5A4C5F5050543031005F564552312E3000086F0000000000000000000000C63D001D55");
-                                        //SetPrinterInfo(RS232Connect.mRecevieData); //
-                                      
-                                        break;
-                                        //case "GN":
-                                        //    SetGNColumnData(RS232Connect.mRecevieData, 3);
-                                        //    break;
-                                        //case "SENSOR":
-                                        //    SetSensorColumnData(RS232Connect.mRecevieData, 3);
-                                        //    break;
+                                    switch (dataType)
+                                    {
+                                        case "PrinterInfo":
+
+                                            break;
+                                    }
                                 }
-                                break;
                             }
-                        }
                             break;
                         case "CommunicationTest": //通訊測試
                             RS232Connect.SerialPortSendCMD("NeedReceive", data, null, 0);
                             RS232ConnectImage.Source = new BitmapImage(new Uri("Images/green_circle.png", UriKind.Relative));
                             RS232Connect.CloseSerialPort(); //沒立刻關閉有時會漏收命令
                             break;
-                        case "Beep":
-                        RS232Connect.SerialPortSendCMD("NoReceive", data, null,0);
+                        case "BeepOrSetting":
+                            RS232Connect.SerialPortSendCMD("NoReceive", data, null, 0);
                             RS232Connect.CloseSerialPort(); //沒立刻關閉有時會漏收命令
                             break;
                     }
-                     
-
-                   
                     if (!RS232Connect.IsConnect)
                     {
                         RS232ConnectImage.Source = new BitmapImage(new Uri("Images/red_circle.png", UriKind.Relative)); //連線失敗時
@@ -307,9 +470,10 @@ namespace PirnterUtility
 
         public static byte[] StringToByteArray(string hex)
         {
-            return Enumerable.Range(0, hex.Length)
+            string afterConvert = hex.Replace(" ", "");
+            return Enumerable.Range(0, afterConvert.Length)
                              .Where(x => x % 2 == 0)
-                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .Select(x => Convert.ToByte(afterConvert.Substring(x, 2), 16))
                              .ToArray();
         }
 
@@ -542,7 +706,10 @@ namespace PirnterUtility
 
 
 
+
+
         #endregion
+
 
     }
 }
