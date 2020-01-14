0c79d0c24918e1274f753b44eb9ee7049f343044
muchone1981 <muchone1981@gmail.com>
2020-01-14 17:37:25 +0800

    新增Codepage代碼類別

---
+++ G80Utility/Tool/CodePage.cs
@@ -0,0 +1,89 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace G80Utility.Tool
+{
+    class CodePage
+    {
+        public static List<string> getCodePageList()
+        {
+            List<string> CPList = new List<string>();
+            CPList.Add("0:PC437");
+            CPList.Add("1:Katakana");
+            CPList.Add("2:PC850");
+            CPList.Add("3:PC860");
+            CPList.Add("4:PC863");
+            CPList.Add("5:PC865");
+            CPList.Add("6:West Europe");
+            CPList.Add("7:Greek");
+            CPList.Add("8:Hebrew");
+            CPList.Add("9:East Europe");
+            CPList.Add("10:lran");
+            CPList.Add("16:WPC1252");
+            CPList.Add("17:PC866");
+            CPList.Add("18:PC852");
+            CPList.Add("19:PC853");
+            CPList.Add("20:lranll");
+            CPList.Add("21:Latvian");
+            CPList.Add("22:Arabic");
+            CPList.Add("23:WPC1251");
+            CPList.Add("24:PC747");
+            CPList.Add("25:WPC1257");
+            CPList.Add("27:Vietnam");
+            CPList.Add("28:PC864");
+            CPList.Add("29:PC1001");
+            CPList.Add("30:Uigur");
+            CPList.Add("31:Hebrew");
+            CPList.Add("32:WPC1255");
+            CPList.Add("33：PC720");
+            CPList.Add("34:WPC1256");
+            CPList.Add("35:WPC1257(N");
+            CPList.Add("255:Thai");
+            CPList.Add("48:WPC1254(T");
+            CPList.Add("50:PC437");
+            CPList.Add("51:Katakana");
+            CPList.Add("52:PC437");
+            CPList.Add("53:PC858");
+            CPList.Add("54:PC852");
+            CPList.Add("55:PC860");
+            CPList.Add("56:PC881");
+            CPList.Add("57:PC863");
+            CPList.Add("58:PC865");
+            CPList.Add("59:PC866");
+            CPList.Add("60:PC855");
+            CPList.Add("61:PC857");
+            CPList.Add("62:PC862");
+            CPList.Add("63:PC864");
+            CPList.Add("64:PC737");
+            CPList.Add("65:PC851");
+            CPList.Add("66:PC869");
+            CPList.Add("67:PC928");
+            CPList.Add("68:PC772");
+            CPList.Add("69:PC774");
+            CPList.Add("70:PC874");
+            CPList.Add("71:WPC1252");
+            CPList.Add("72:WPC1250");
+            CPList.Add("73:WPC1251");
+            CPList.Add("74:PC3840");
+            CPList.Add("75:PC3841");
+            CPList.Add("76:PC3843");
+            CPList.Add("77:PC3844");
+            CPList.Add("78:PC3845");
+            CPList.Add("79:PC3846");
+            CPList.Add("80:PC3847");
+            CPList.Add("81:PC3848");
+            CPList.Add("82:PC1001");
+            CPList.Add("83:PC2001");
+            CPList.Add("84:PC3001");
+            CPList.Add("85:PC3002");
+            CPList.Add("86:PC3011");
+            CPList.Add("87:PC3012");
+            CPList.Add("88:PC3021");
+            CPList.Add("89:PC3041");
+
+            return CPList;
+        }
+
+    }
+}
