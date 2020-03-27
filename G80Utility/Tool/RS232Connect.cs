425546efb91f73bfeedeedd53149d19a76f39884
muchone1981 <muchone1981@gmail.com>
2020-03-27 11:10:35 +0800

    rs232 timeout時間從5000改為500

--- G80Utility/Tool/RS232Connect.cs
+++ G80Utility/Tool/RS232Connect.cs
@@ -28,8 +28,8 @@ namespace G80Utility.Tool
             mSerialPort.DataBits = 8;
             mSerialPort.StopBits = StopBits.One;
             mSerialPort.Handshake = Handshake.RequestToSend;
-            mSerialPort.ReadTimeout = 5000;
-            mSerialPort.WriteTimeout = 5000;
+            mSerialPort.ReadTimeout = 500;
+            mSerialPort.WriteTimeout = 500;
             //不加下面兩行就會無法開啟serialport
             mSerialPort.DtrEnable = true;
             mSerialPort.RtsEnable = true;
